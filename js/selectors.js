aa06d15f025bf7db2bb53b3927b197043d6ce56e
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-16 15:35:49 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,5 +8,13 @@ function lastImageSelector() {
 }
 
 function ninjaBabySelector() {
-  return $("#")
+  return $("#baby-ninja");
+}
+
+function divSelector() {
+  return $("div.pics");
+}
+
+function firstListItem() {
+  return $("ul#pic-list li:first-child");
 }
